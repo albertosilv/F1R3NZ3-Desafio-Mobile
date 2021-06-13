e9a3e2ae09d0c0546eecfe1ed159c3586cd6fedd
albertosilv <jose.alberto.silva@ccc.ufcg.edu.br>
2021-06-13 20:42:05 -0300

    profile create

--- src/Pages/Profile/Styles.js
+++ src/Pages/Profile/Styles.js
@@ -4,12 +4,31 @@ export const style = StyleSheet.create({
     background: {
         flex: 1,
     },
-    icon:{
+    like:{
         borderRadius:100,
         padding:15,
         borderColor:'#9899ad',
-        borderWidth:1
+        borderWidth:1,
+        backgroundColor:'#fff',
+        left:70,
+    },
+    check:{
+        position:'absolute',
+        top:135,
+        left:120,
+        backgroundColor:'#441d92',
+        borderColor:'#441d92',
+        borderRadius:100,
+        color:'#fff',
+    },
+    whatsapp:{
+        color:'#fff',
+        backgroundColor:'#40c351',
+        borderRadius:100,
+        borderWidth:1,
+        borderColor:'#fff'
     }
+
 })
 export const Container = styled.View`
     flex:1;
@@ -17,7 +36,7 @@ export const Container = styled.View`
     background-color:#fff
     
 `
-export const Fundo = styled.ImageBackground`
+export const BackgroundUser = styled.ImageBackground`
     flex:0.4;
     flex-direction:row-reverse;
     position: absolute;
@@ -39,14 +58,14 @@ export const Header = styled.View`
     position:absolute;
     top:23%;
 `
-export const LikeContainer = styled.View`
-    background:#fff,
-    border-radius:100px;
-    right:2%;
-`
-export const UserContainer = styled.View`
+export const ContainerOne = styled.View`
     flex:1 
     left:5%;
+`
+export const ContainerTwo = styled.View`
+    flex-direction:column;
+    right:24%;
+    width:90px;
 `
 export const User = styled.Image`
     border-radius:100px;
@@ -55,4 +74,73 @@ export const User = styled.Image`
     overflow: hidden;
     border-width: 10px;
     border-color: #fff;
+`
+export const UserName = styled.Text`
+    font-size:30px;
+    width:60%;
+    font-weight: 600;
+
+`
+export const Instagram = styled.Text`
+    font-size:15px;
+    width:60%;
+    color:#5c18e4
+    padding-top:10px;
+    padding-bottom:10px;
+`
+export const Localization = styled.Text`
+    font-size:18px;
+    width:70%;
+    font-style: italic;
+    font-weight: 100;
+`
+
+export const InformationContainer = styled.View`
+    flex-direction:row;
+`
+export const Information  = styled.View`
+    flex-direction:column;
+    margin:7px;
+`
+
+export const Data = styled.Text`
+    font-size:20px;
+    font-weight: 700;
+    color:#61626b;
+    text-align:center;
+`
+export const Description = styled.Text`
+    font-size:20px;
+    font-weight: 200;
+    color:#61626b;
+`
+export const AddFriends = styled.TouchableOpacity`
+    flex-direction:row;
+    margin-top:20px;
+    background-color:#441d92;
+    color:#fff;
+    width:170px;
+    height:50px;
+    align-items:center;
+    justify-content:center
+    border-radius:7px;
+ `
+ export const Text = styled.Text`
+    color:#fff
+    font-size:17px;
+`
+export const Whatsapp = styled.View`
+    flex-direction:row;
+    margin-top:25px;
+`
+export const WhatsappNumber = styled.Text`
+    width:170px;
+    font-size:15px;
+`
+
+export const Bio = styled.Text`
+    top:20%
+    text-align:center;
+    font-size:28px;
+    font-style: italic;
 `
--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -1,26 +1,72 @@
 import React from 'react'
-import { } from 'react-native'
-import { Container, style, Fundo, Section, ButtonLogout, Header, LikeContainer, UserContainer, User } from './Styles'
+import {  } from 'react-native'
+import {
+    Container,
+    style,
+    BackgroundUser,
+    Section,
+    ButtonLogout,
+    Header,
+    ContainerOne,
+    ContainerTwo,
+    User,
+    UserName,
+    Instagram,
+    Localization,
+    Information,
+    Data,
+    Description,
+    InformationContainer,
+    AddFriends,
+    Text,
+    Whatsapp,
+    WhatsappNumber,
+    Bio
+} from './Styles'
 import Icon from 'react-native-vector-icons/SimpleLineIcons'
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5'
 import { LinearGradient } from 'expo-linear-gradient';
-const fundo = "https://i.pinimg.com/originals/a3/ba/fc/a3bafc289e3752bc688d25c3dc5fc7ad.jpg"
+const backgroundUser = "https://i.pinimg.com/originals/a3/ba/fc/a3bafc289e3752bc688d25c3dc5fc7ad.jpg"
 const user = "https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png"
 function Profile({ navigation }) {
     return (
         <LinearGradient colors={['#d3b3a8', '#42354d']} style={style.background}>
             <Container>
-                <Fundo source={{ uri: fundo }}>
+                <BackgroundUser source={{ uri: backgroundUser }}>
                     <ButtonLogout onPress={() => navigation.navigate('Login')}><Icon name="logout" color="#9293a8" size={30} /></ButtonLogout>
-                </Fundo>
-                <Section></Section>
+                </BackgroundUser>
+                <Section>
+                </Section>
                 <Header>
-                    <UserContainer>
-                        <User source={{ uri: fundo }} />
-                    </UserContainer>
-                    <LikeContainer>
-                        <Icon name="heart" size={60} style={style.icon} />
-                    </LikeContainer>
+                    <ContainerOne>
+                        <User source={{ uri: backgroundUser }} />
+                        <MaterialIcon name="check" size={35} style={style.check} />
+                        <UserName>Joana Silva </UserName>
+                        <Instagram>@joanasilva_1</Instagram>
+                        <Localization>Campina Grande</Localization>
+                        <Localization>PB, Brasil</Localization>
+                    </ContainerOne>
+                    <ContainerTwo>
+                        <Icon name="heart" size={60} style={style.like} />
+                        <InformationContainer>
+                            <Information>
+                                <Data>236</Data>
+                                <Description>Likes</Description>
+                            </Information>
+                            <Information>
+                                <Data>550</Data>
+                                <Description>Amigos</Description>
+                            </Information>
+                        </InformationContainer>
+                        <AddFriends title="Add Amigos"><FontAwesome5Icon color="#fff"name="user-friends" size={26}/><Text> Add Amigos</Text></AddFriends>
+                        <Whatsapp>
+                            <MaterialIcon name="whatsapp" size={25} style={style.whatsapp}/>
+                            <WhatsappNumber>99 99999-9999</WhatsappNumber>
+                        </Whatsapp>
+                    </ContainerTwo>
                 </Header>
+                <Bio>" Especialidades em comerciais  de moda "</Bio>
             </Container>
         </LinearGradient>
     )
